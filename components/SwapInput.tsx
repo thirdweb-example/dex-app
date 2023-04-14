c4522a73294cfbb01cdb5e13523a1d1021a7b13e
Atharva Deosthale <atharva.deosthale17@gmail.com>
2023-04-14 21:10:47 +0530

    Initial commit

---
+++ components/SwapInput.tsx
@@ -0,0 +1,37 @@
+import { HStack, Image, Input } from "@chakra-ui/react";
+import { resolveIpfsUri } from "@thirdweb-dev/react";
+import React from "react";
+
+type Props = {
+  type: "native" | "token";
+  tokenImage?: string;
+  current: string;
+  setValue: (value: string) => void;
+  value: string;
+};
+
+export default function SwapInput({
+  type,
+  tokenImage,
+  setValue,
+  value,
+  current,
+}: Props) {
+  return (
+    <HStack w="full" bgColor="gray.100" rounded="2xl" px="5">
+      <Image src={tokenImage || "/token.png"} w="7" h="7" />
+      <Input
+        type="number"
+        placeholder="0.0"
+        fontSize="3xl"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        outline="none"
+        py="10"
+        isDisabled={current !== type}
+        border="none"
+        fontFamily="monospace"
+      />
+    </HStack>
+  );
+}
